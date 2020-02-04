80b8bc1f0f8a9143a03e424e41fed3a397509d3d
Donadonzi <donadonzi@gmail.com>
2020-02-03 23:50:52 -0500

    adding the blog project

---
+++ blog/src/reducers/index.js
@@ -0,0 +1,5 @@
+import { combineReducers } from 'redux';
+
+export default combineReducers({
+    replaceMe: () => 'YooHoo!!' // We just add this to trick the broweser into thinking we do have some reducers, to make that err msg go away!
+});
