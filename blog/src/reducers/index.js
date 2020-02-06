76c2742eb555272748492d94253bde275ab37459
Donadonzi <donadonzi@gmail.com>
2020-02-05 23:21:40 -0500

    adding postsReducer to the blog project

--- blog/src/reducers/index.js
+++ blog/src/reducers/index.js
@@ -1,5 +1,6 @@
 import { combineReducers } from 'redux';
+import postsReducer from './postsReducer';
 
 export default combineReducers({
-    replaceMe: () => 'YooHoo!!' // We just add this to trick the broweser into thinking we do have some reducers, to make that err msg go away!
+    posts: postsReducer
 });
