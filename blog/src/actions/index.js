d13596eb89e42a036e8e60ff320a6bf2f2026b39
Donadonzi <donadonzi@gmail.com>
2020-02-07 17:48:49 -0500

    working on users part - action creator and reducer

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -23,7 +23,12 @@ export const fetchPosts = () => { // Removed async
 // Shortened code equal to the above code, ES2015 syntax
 export const fetchPosts = () => async dispatch =>  {
         const response  =  await jsonPlaceholder.get('/posts');
-        dispatch( { type: 'FETCH_POSTS', payload: response });    
-    };
+        dispatch( { type: 'FETCH_POSTS', payload: response.data });    
+};
+
+export const fetchUser = id => async dispatch => {
+    const response = await jsonPlaceholder.get(`users/${id}`);
+    dispatch( { type: 'FETCH_USER', payload: response.data });
+};
 
 
--- blog/src/components/PostList.js
+++ blog/src/components/PostList.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 import { fetchPosts } from '../actions';
+import UserHeader from './UserHeader';
 
  class PostList extends React.Component {
 
@@ -9,14 +10,35 @@ import { fetchPosts } from '../actions';
         this.props.fetchPosts();
     }
 
-     render() {
+    renderList () {
+        return this.props.posts.map(post => {
+            return (
+                <div className="item" key={post.id}>
+                    <i className="large middle aligned icon user" />
+                    <div className="content">
+                        <div className="description">
+                            <h2>{post.title}</h2>
+                            <p>{post.body}</p>
+                        </div>
+                        <UserHeader userId={post.userId} />
+                    </div>
+                </div>
+            );
+        });
+    }
+
+    render() {
         return (
-            <div>
-                Post List!
+            <div className="ui relaxed divided list">
+                {this.renderList()}
             </div>
         );
-     }
+    }
  }
 
+ const mapStateToProps = state => {
+    return { posts: state.posts };
+ }; 
+
  
- export default connect(null, { fetchPosts : fetchPosts })(PostList); // First argument of connect() is always mapStateToProps(). But since we don't have it now, we pass null
+ export default connect(mapStateToProps, { fetchPosts : fetchPosts })(PostList); // First argument of connect() is always mapStateToProps(). But since we don't have it now, we pass null
