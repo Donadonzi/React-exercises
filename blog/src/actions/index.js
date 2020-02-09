622265c033fd31321a6eaf22849cc70ad39ae9da
Donadonzi <donadonzi@gmail.com>
2020-02-08 19:15:36 -0500

    memoizing fetchUser action creator

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,4 +1,7 @@
+import _ from 'lodash';
+
 import jsonPlaceholder from '../apis/jsonPlaceholder';
+
 
 /*
 export const fetchPosts = () => { // Removed async 
@@ -26,9 +29,13 @@ export const fetchPosts = () => async dispatch =>  {
         dispatch( { type: 'FETCH_POSTS', payload: response.data });    
 };
 
-export const fetchUser = id => async dispatch => {
+export const fetchUser = id => dispatch => {
+    _fetchUser(id, dispatch);
+};
+
+const _fetchUser = _.memoize(async (id, dispatch) => {
     const response = await jsonPlaceholder.get(`users/${id}`);
     dispatch( { type: 'FETCH_USER', payload: response.data });
-};
+});
 
 
