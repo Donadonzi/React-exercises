14ae87c764c804a2d62584fce83fac68555834ce
Donadonzi <donadonzi@gmail.com>
2020-02-08 20:00:49 -0500

    finished blog project

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -29,6 +29,14 @@ export const fetchPosts = () => async dispatch =>  {
         dispatch( { type: 'FETCH_POSTS', payload: response.data });    
 };
 
+/* Commented out this version to implement the memoized version
+export const fetchUser = id => async dispatch => {
+    const response = await jsonPlaceholder.get(`users/${id}`);
+    dispatch( { type: 'FETCH_USER', payload: response.data });
+};
+*/
+
+
 export const fetchUser = id => dispatch => {
     _fetchUser(id, dispatch);
 };
@@ -37,5 +45,6 @@ const _fetchUser = _.memoize(async (id, dispatch) => {
     const response = await jsonPlaceholder.get(`users/${id}`);
     dispatch( { type: 'FETCH_USER', payload: response.data });
 });
+
 
 
